04bf727fe4cb3a0c7353158e068359bab6c3c85b
jai-gohash <mathurjai114@gmail.com>
2026-05-14 00:02:21 +0530

    Updates

--- assets/Navbar-wlWqcQ0N.js
+++ assets/Navbar-wlWqcQ0N.js
@@ -1,4 +1,4 @@
-import{j as P,r as bt}from"./index-BEd0h2rY.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
+import{j as P,r as bt}from"./index-BN9iKK7v.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
  * SplitText 3.15.0
  * https://gsap.com
  *
