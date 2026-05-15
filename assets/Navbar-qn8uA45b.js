2dcb8f43a0a83811221ea257dd4832354f2f2234
jai-gohash <mathurjai114@gmail.com>
2026-05-15 21:21:53 +0530

    Updates

--- assets/Navbar-qn8uA45b.js
+++ assets/Navbar-qn8uA45b.js
@@ -1,4 +1,4 @@
-import{j as P,r as bt}from"./index-DlpURj2q.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
+import{j as P,r as bt}from"./index-BA2Z2ub_.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
  * SplitText 3.15.0
  * https://gsap.com
  *
